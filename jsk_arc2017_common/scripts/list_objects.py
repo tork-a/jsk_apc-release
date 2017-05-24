96a4d86d3df6c72537fa79d5e0758fa72497d0b1
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-05-25 03:10:20 +0900

    Revert "Trimmed the branch to only the jsk_apc2015_common sub directory"
    This reverts commit 502b31b4c5288c04010efeb0c20b67f905ce4bab.

---
+++ jsk_arc2017_common/scripts/list_objects.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import os.path as osp
+
+import rospkg
+import yaml
+
+
+PKG_DIR = rospkg.RosPack().get_path('jsk_arc2017_common')
+
+with open(osp.join(PKG_DIR, 'config/label_names.yaml')) as f:
+    object_names = yaml.load(f)['label_names']
+
+for obj_id, obj in enumerate(object_names):
+    print('%2d: %s' % (obj_id, obj))
