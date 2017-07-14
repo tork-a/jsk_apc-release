4ea363fe2b2ac0c3cc94e4eae6ac851469472c9f
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-07-15 05:35:05 +0900

    Rebase from 'upstream'

--- jsk_arc2017_common/node_scripts/ekew_i_driver.py
+++ jsk_arc2017_common/node_scripts/ekew_i_driver.py
@@ -2,8 +2,8 @@
 
 import serial
 
+from jsk_arc2017_common.msg import WeightStamped
 import rospy
-from std_msgs.msg import Float32
 
 
 class EkEwIDriver(object):
@@ -16,11 +16,13 @@ class EkEwIDriver(object):
     def __init__(self):
         super(EkEwIDriver, self).__init__()
         port = rospy.get_param('~port', '/dev/ttyUSB0')
+        timeout = rospy.get_param('~timeout', None)
         rospy.loginfo('port=%s', port)
         # EK-i/EW-i series default settings
         self.ser = serial.Serial(
-            port, baudrate=2400, bytesize=7, parity=serial.PARITY_EVEN)
-        self.pub = rospy.Publisher('~output', Float32, queue_size=1)
+            port, baudrate=2400, bytesize=7, parity=serial.PARITY_EVEN,
+            timeout=timeout, writeTimeout=timeout)
+        self.pub = rospy.Publisher('~output', WeightStamped, queue_size=1)
         rate = rospy.get_param('~rate', 10)
         self.read_timer = rospy.Timer(rospy.Duration(1. / rate),
                                       self._read_timer_cb)
@@ -29,30 +31,41 @@ class EkEwIDriver(object):
         if self.pub.get_num_connections() == 0:
             return
 
-        self.ser.write('Q\r\n')
+        try:
+            self.ser.write('Q\r\n')
+        except SerialTimeoutException:
+            rospy.logerr('Serial write timeout')
+            rospy.signal_shutdown('Serial write timeout')
+            return
         data = self.ser.read(17)
+        if len(data) != 17:
+            rospy.logerr('Serial read timeout')
+            rospy.signal_shutdown('Serial read timeout')
+            return
 
         header = data[:2]
+        weight = -1  # unknown
         if header == 'ST':
             # scale mode
-            weight = float(data[3:12])
             unit = data[12:15]
             if unit != '  g':
                 rospy.logerr('Unsupported unit: %s', unit)
-                return
-            msg = Float32(data=weight)
-            self.pub.publish(msg)
+            else:
+                weight = float(data[3:12])
         elif header == 'QT':
             # number mode
             rospy.logerr('Unsupported mode: %s', header)
-            return
         elif header == 'US':
             # unstable
-            return
+            pass
         elif header == 'OL':
             # scale over
             rospy.logerr('Scale over')
-            return
+
+        msg = WeightStamped()
+        msg.header.stamp = event.current_real
+        msg.weight.value = weight
+        self.pub.publish(msg)
 
 
 if __name__ == '__main__':
